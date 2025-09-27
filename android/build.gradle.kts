3a0286cb46fa72f7ea2db38d168f400911f6a7db
Abdullah Talha <153621009+abdullahtalha22@users.noreply.github.com>
2025-09-27 08:54:36 +0500

    Add files via upload

---
+++ android/build.gradle.kts
@@ -0,0 +1,38 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    // ...
+
+    // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.4.3" apply false
+
+}
+
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.6.1") // ✅ Latest Android Gradle Plugin (AGP)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+        classpath ("com.google.gms:google-services:4.4.3")// ✅ Updated Kotlin version
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
+
